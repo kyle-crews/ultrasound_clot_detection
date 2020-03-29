25e531d3a4d85565eac3f90d99adaf08d6ee5423
github username <github email address>
2020-03-29 02:59:14 -0500

    added functionality related to detecto for python

---
+++ python_detecto/detecto_demo_0.py
@@ -0,0 +1,10 @@
+# import detecto's modules
+from detecto import core, utils, visualize
+
+# read the image and initialize a pre-trained model
+image = utils.read_image('zebra.jpg')
+model = core.Model()
+
+# generate and plot the top perditions
+labels, boxes, scores = model.predict_top(image)
+visualize.show_labeled_image(image, boxes, labels)
